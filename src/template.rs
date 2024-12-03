0e9a64e9919f985fb6e34b3e78a1dba07202c98d
Johannes <freelon@gmx.net>
2024-12-03 06:55:12 +0100

    Prepare day 4 and template.

---
+++ src/template.rs
@@ -0,0 +1,35 @@
+use std::fs::read_to_string;
+
+pub fn day_main() {
+    let input = read_to_string("input/dayXX.txt").unwrap();
+    let input = input.trim();
+    println!(" part1: {}", part1(input));
+    println!(" part2: {}", part2(input));
+}
+
+type RiddleResult = i64;
+
+fn part1(input: &str) -> RiddleResult {
+    0
+}
+
+fn part2(_input: &str) -> RiddleResult {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::{part1, part2};
+
+    const TEST_INPUT: &str = r"";
+
+    #[test]
+    fn test1() {
+        assert_eq!(part1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(part2(TEST_INPUT), 0);
+    }
+}
